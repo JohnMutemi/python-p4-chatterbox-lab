780e8cabe48044dfd340a5d65c4260ba59c15027
JohnMutemi <jmtutorsalp@gmail.com>
2024-07-26 05:44:57 +0300

    commit backend

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, make_response, jsonify
+
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    if message:
+        return jsonify(message.to_dict())
+    else:
+        return jsonify({"error": "Message not found"}), 404
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.get(id)
+    if message is None:
+        return jsonify({"error": "Message not found"}), 404
+    message.body = data.get('body', message.body)
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if message is None:
+        return jsonify({"error": "Message not found"}), 404
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({"message": "Message deleted"}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
